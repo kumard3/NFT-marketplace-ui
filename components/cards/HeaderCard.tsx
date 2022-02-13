c3353bc72bd423c82d62639dba2745873179fab9
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-14 00:45:01 +0530

    card component added

---
+++ components/cards/HeaderCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeaderCard() {
+  return (
+    <div className="w-[280px] h-[186px]">
+        <img src="/Art.png" />
+    </div>
+  )
+}
---
+++ components/home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl">NFT MARKER PLACE</h1>
+      </div>
+      <div>
+          <h1 className="text-3xl">Treanding Collections </h1>
+
+      </div>
+    </div>
+  );
+}
